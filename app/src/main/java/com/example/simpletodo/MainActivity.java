2b438cf3e1808e3d43d1e080edffce11b9096b43
Mohammed Rahman <minhaz357@gmail.com>
2021-12-16 21:37:22 -0500

    Implement Model as List of String and create mock data

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.simpletodo;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+    Button btnAdd;
+    EditText editText;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        editText = findViewById(R.id.editItem);
+        recyclerView = findViewById(R.id.rvItems);
+
+        editText.setText("Please, enter your item!");
+
+        // create some mock data
+        items = new ArrayList<>();
+        items.add("Solve Leetcode problems");
+        items.add("Exercise for 30m");
+        items.add("Go to work");
     }
 }
