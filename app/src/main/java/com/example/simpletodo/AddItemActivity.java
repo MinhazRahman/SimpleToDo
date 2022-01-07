928594e0060a373d62d4fbd3bb24a2523d3f8cba
Mohammed Rahman <minhaz357@gmail.com>
2022-01-06 22:49:57 -0500

    Add DatePicker and TimePicker dialogs

--- app/src/main/java/com/example/simpletodo/AddItemActivity.java
+++ app/src/main/java/com/example/simpletodo/AddItemActivity.java
@@ -2,22 +2,33 @@ package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Calendar;
 import java.util.Objects;
 
 public class AddItemActivity extends AppCompatActivity {
 
-    EditText txtAddItem;
+    EditText editTextAddItem;
+    EditText editTextDate;
+    EditText chooseTime;
     FloatingActionButton btnDone;
     FloatingActionButton btnCancel;
+    DatePickerDialog datePickerDialog;
+    TimePickerDialog timePickerDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +36,77 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
 
         // get the elements
-        txtAddItem = findViewById(R.id.txtAddItem);
+        editTextAddItem = findViewById(R.id.txtAddItem);
         btnDone = findViewById(R.id.btnAddItemDone);
         btnCancel = findViewById(R.id.btnAddItemCancel);
+        editTextDate = findViewById(R.id.editTxtDate);
+        chooseTime = findViewById(R.id.editTxtChooseTime);
+
 
         Objects.requireNonNull(getSupportActionBar()).setTitle("Add Item");
+
+        // Pop up date picker dialog when clicked on editTxtDate text field
+        // Enter the selected date to the text field
+        editTextDate.setInputType(InputType.TYPE_NULL);
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.append(dayOfMonth).append("/").append((monthOfYear + 1)).append("/").append(year);
+                        editTextDate.setText(stringBuilder);
+                    }
+                };
+
+                // DatePicker dialog
+               datePickerDialog = new DatePickerDialog(AddItemActivity.this,dateSetListener, year, month, day);
+               datePickerDialog.show();
+            }
+        });
+
+        // Pop up the timer when clicked on the Choose time text field
+        // and user can use the time from the timer
+        chooseTime.setOnClickListener(new View.OnClickListener() {
+            // define Android Time Picker inside onClick method
+            @Override
+            public void onClick(View view) {
+
+                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        // Show selected time as am-pm inside EditText field
+                        String amPm;
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        StringBuilder selectedTime = new StringBuilder();
+                        selectedTime.append(hourOfDay).append(":").append(minutes).append(amPm);
+                        chooseTime.setText(selectedTime);
+                    }
+                };
+
+                // Create the TimePickerDialog
+                timePickerDialog = new TimePickerDialog(AddItemActivity.this, timeSetListener, 0, 0, false);
+                timePickerDialog.show();
+
+            }
+        });
 
         // After entering the item on the text field, we click on the done button
         btnDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Get the text from the text field
-                String item = txtAddItem.getText().toString().trim();
+                String item = editTextAddItem.getText().toString().trim();
 
                 // Create an Intent
                 Intent intent = new Intent();
